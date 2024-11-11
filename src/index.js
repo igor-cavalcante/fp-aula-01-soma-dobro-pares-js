6d746c8dbd06a0af4ec80e626bc0a2af35df1c2b
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2024-11-11 12:05:12 -0300

    implementando as funções

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 
 import * as math from './math.js';
 
-const numeros = [1, 2, 3, 4, 5, 6]; // escolha os números que desejar 
+const numeros = [1, 4, 3, 6, 5, 8,10,18]; // escolha os números que desejar 
 console.log('Soma usando a abordagem funcional:', math.somaDobroNumerosParesFuncional(numeros))
 console.log('Soma usando a abordagem estrutural (procedural):', math.somaDobroNumerosParesProcedural(numeros))
--- src/math.js
+++ src/math.js
@@ -6,18 +6,29 @@
  * Realiza a soma do dobro do valor de cada número par em um vetor de números.
  * A função deve ser implementada utilizando a abordagem funcional com o modelo map/reduce.
  * @param {Array} numeros vetor com os números desejados
+ * @returns {number} - A soma do dobro dos números pares
  */
-function somaDobroNumerosParesFuncional(numeros){
-    // TODO: Insira seu código funcional aqui
+function somaDobroNumerosParesFuncional(numeros) {
+    return numeros
+        .filter(num => num % 2 === 0) // Filtra apenas os números pares
+        .map(num => num * 2)          // Dobra cada número par
+        .reduce((acc, num) => acc + num, 0); // Soma todos os valores dobrados
 }
 
 /**
  * Realiza a soma do dobro do valor de cada número par em um vetor de números.
  * A função deve ser implementada utilizando a abordagem estruturada (procedural) convencional.
  * @param {Array} numeros vetor com os números desejados
+* @returns {number} - A soma do dobro dos números pares
  */
-function somaDobroNumerosParesProcedural(numeros){
-    // TODO: Insira seu código estruturado aqui   
+function somaDobroNumerosParesProcedural(numeros) {
+    let soma = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {      // Verifica se o número é par
+            soma += numeros[i] * 2;      // Dobra o número e adiciona à soma
+        }
+    }
+    return soma;
 }
 
 export { somaDobroNumerosParesFuncional, somaDobroNumerosParesProcedural };
